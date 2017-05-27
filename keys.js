ee1832962a4b2dd6ebb166e7b6d65a2da1d11209
Kevin Jendreas <kjendreas@gmail.com>
2017-05-26 19:25:20 -0700

    project upload

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'UPGLv81UZKmtEJCem2k9aOs2x',
+  consumer_secret: 'C6GPCBMEXWFLwaqiFjRsNMi66cFJYyMRyuZnTrf9BdutYouQ4b',
+  access_token_key: '4806476433-jvVGcn0M08HudKNaAnUNAIi2UrdtGdY3gBPOJvb',
+  access_token_secret: 'HOcCVzpvhWdDaLdQ2bcq2TXwetxUOuTGUPIMw66HbP7cn',
+}
